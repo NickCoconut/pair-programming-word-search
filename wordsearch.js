8daf2b56d642ffeae94f24f83980cb1bd60056d6
NickCoconut <gelaxyy@gmail.com>
2021-12-02 02:47:25 +0000

    modified wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,19 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) {
+            return true
+        }
     }
+    return false;
 }
 
 module.exports = wordSearch
+
+
+// const wordSearch = (array, word) => {
+//     find the word horizontally and vertically
+//     if found
+//     return true 
+//     else return false
+//   }
